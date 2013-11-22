5a127a8aa8bab43b2ffcc92b1508a4ce15703d92
James Waples <jamwaffles@gmail.com>
2013-11-22 14:59:25 +0000

    Generate 2D array to render calendar table with

--- supercal.js
+++ supercal.js
@@ -18,28 +18,121 @@
 		startDay: 1,		// Start day. 0 = Sunday, 1 = Monday, etc
 	};
 
-	// Generate a 2D array of `Date` objects ready to generate an HTML table from
-	function tableDates(inputDate) {
-		var selectedDate;
+	Date.prototype.daysInMonth = function(){
+		var d = new Date(this.getFullYear(), this.getMonth() + 1, 0);
+		
+		return d.getDate();
+	}
 
-		if(inputDate !== undefined) {
-			selectedDate = inputDate;
+	// Class defining a single table cell. Should be stored in an array in an instance of `Month`
+	function Day(date) {
+		if(date !== undefined) {
+			tmpDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0, 0);
 		} else {
-			selectedDate = new Date(0);
+			tmpDate = new Date();
+
+			tmpDate.setHours(0);
+			tmpDate.setMinutes(0);
+			tmpDate.setSeconds(0);
+			tmpDate.setMilliseconds(0);
 		}
 
-		// This is a _day_ not a time
-		selectedDate.setHours(0);
-		selectedDate.setMinutes(0);
-		selectedDate.setSeconds(0);
-		selectedDate.setMilliseconds(0);
-
-		var dates = [];
-
-		// for(var ) {
-			
-		// }
+		this.date = tmpDate;
+		this.today = new Date();
 	}
+	Day.prototype.isToday = function() {
+		return this.date.getFullYear() === this.today.getFullYear() &&
+				this.date.getMonth() === this.today.getMonth() &&
+				this.date.getDate() === this.today.getDate();
+	}
+
+	// Create a month object from any given date
+	function Month(date, options) {
+		this.days = [];
+		this.grid = [];
+		this.options = options;
+		this.date = new Date(date.getTime());
+
+		this.date.setHours(0);
+		this.date.setMinutes(0);
+		this.date.setSeconds(0);
+		this.date.setMilliseconds(0);
+
+		var day = date;
+		var monthNumber = day.getMonth();			// The number of this month, used to break `while()` loop
+		day.setDate(1);			// Reset to the first day
+
+		while(day.getMonth() === monthNumber) {
+			this.days.push(new Day(day));
+
+			day.setDate(day.getDate() + 1);
+		}
+	}
+
+	// Get a day by index (E.g. get previous day from previous month 4 days from end of month: `Month.getDay(-4)`)
+	Month.prototype.getDay = function(index) {
+		if(index > 0) {
+			return new Day(new Date(this.date.getFullYear(), this.date.getMonth(), index));
+		} else {
+			return new Day(new Date(this.date.getFullYear(), this.date.getMonth(), this.date.daysInMonth() + index));
+		}
+	}
+
+	// Produce a 2D array of cells to create a calendar table for this month
+	Month.prototype.cells = function() {
+		var day = new Date(this.date.getTime());
+		day.setDate(1);
+
+		var numDaysPreceding = day.getDay() - this.options.startDay;
+		var numDaysAfter = numDaysPreceding + this.date.daysInMonth() - 42;
+
+		var prevMonth = new Month(new Date(this.date.getFullYear(), this.date.getMonth() - 1, this.date.getDate(), 0, 0, 0, 0));
+		var nextMonth = new Month(new Date(this.date.getFullYear(), this.date.getMonth() + 1, this.date.getDate(), 0, 0, 0, 0));
+
+		var index = 1;
+
+		for(var row = 0; row < 6; row++) {
+			var rowCells = [];
+
+			for(var col = 0; col < 7; col++) {
+				var cell;
+
+				// Previous month
+				if(index <= numDaysPreceding) {
+					cell = prevMonth.getDay(-numDaysPreceding + index);
+				} else if(index > numDaysPreceding + this.date.daysInMonth()) {		// Next month
+					cell = nextMonth.getDay(index - numDaysPreceding - this.date.daysInMonth());
+				} else {		// Current month
+					cell = this.getDay(index - numDaysPreceding);
+				}
+
+				rowCells.push(cell);
+
+				index++;
+			}
+
+			this.grid.push(rowCells);
+		}
+
+		return this.grid;
+	}
+
+	// Generate a 2D array of `Date` objects ready to generate an HTML table from
+	// function tableDates(inputDate) {
+	// 	var selectedDate;
+
+	// 	if(inputDate !== undefined) {
+	// 		selectedDate = inputDate;
+	// 	} else {
+	// 		selectedDate = new Date();
+	// 	}
+
+	// 	var lastMonth = new Month(new Date(inputDate.getFullYear(), inputDate.getMonth() - 1, inputDate.getDate()));
+	// 	var thisMonth = new Month(inputDate);
+	// 	var nextMonth = new Month(new Date(inputDate.getFullYear(), inputDate.getMonth() + 1, inputDate.getDate()));
+
+
+	// }
 
 	var methods = {
 		// Create a Supercal instance for this element from scratch
@@ -47,7 +140,10 @@
 		init: function(passedOptions) {
 			var options = $.extend({}, defaults, passedOptions);
 
-			console.log(options);
+			var foo = new Month(new Date(), options);
+
+			console.log(foo.cells());
+			// console.log(options);
 		}
 	};
 
